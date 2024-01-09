53b278deb9a0c96c87ad3c2e00175072458088fc
Nikhil-0200 <nikhiltiwari0200@gmail.com>
2024-01-10 04:47:57 +0530

    This is my React First Assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let ele = <div id="parent">
+<span>Parent</span>
+<div>1</div>
+<div>2</div>
+<div>3</div>
+<div>4</div>
+
+</div>
+
+
+    let reactRoot = ReactDOM.createRoot(document.getElementById("root"));
+
+    reactRoot.render(ele);
